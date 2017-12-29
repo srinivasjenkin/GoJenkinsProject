fc0452fa85bfe406ffe32ef2acedc97056d05a27
Rao Koruprolu <s.rao.koruprolu@accenture.com>
2017-12-29 11:20:18 +0530

    Initial maven first project

---
+++ src/main/java/SampleProject/Myfirstmaventest/App.java
@@ -0,0 +1,13 @@
+package SampleProject.Myfirstmaventest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
